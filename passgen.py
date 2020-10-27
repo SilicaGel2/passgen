fee18cbee625772ffaa9ea685e9ec754a2aa2f79
Richard Benjamin <richardbenjaminjr@gmail.com>
2020-10-26 21:33:45 -0400

    Refactor way to get word list

--- passgen.py
+++ passgen.py
@@ -1,2 +1,19 @@
 # this is a comment
-print ('Horse-pillow-incendiary')
+
+def get_words():
+	return ["adult", "zygon", "cancer", "campus", "yeti", "thunder"]
+
+words = get_words()
+top_3 = words[0:3]
+
+#print(words[0:3])
+print(top_3)
+sep = '-'
+print(sep.join(top_3))
+#def my_function(name):
+  #print("Hello " + name)
+
+#my_function('Richard')
+
+
+
