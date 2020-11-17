ac1a0d3e555aa5170905c0bd94054a0ec2045b41
Richard Benjamin <richardbenjaminjr@gmail.com>
2020-11-16 20:34:26 -0500

    restructured the user input for MAX/MIN length

--- passgen.py
+++ passgen.py
@@ -5,31 +5,52 @@ wordLenMin = 5
 wordLenMax = 10
 badWords = ["ass", "poop", "fucking", "asshole"]
 
+def prompt_for_integer(prompt):
+	rawInput = input(prompt)
+	try:
+		parsed = int(rawInput)
+		return parsed
+	except ValueError:
+		return None
 
-if len(sys.argv) == 2:
-	wordLenMin = int(sys.argv[1])
-elif len(sys.argv) > 2:
-	wordLenMin = int(sys.argv[1])
-	wordLenMax = int(sys.argv[2])		
+
+def int_check(prompt, error):
+	intInput = prompt_for_integer(prompt)
+	while intInput is None:
+		print(error)
+		intInput = prompt_for_integer(prompt)
+	return intInput
+
+	
+wrdMinInput = int_check("What is the smallest word length ", "Invalid input! You did not provide an integer")
+wordLenMin = wrdMinInput
+print("shortest word length is: " + str(wordLenMin))
+# set shortest length
+
+wrdMaxInput = int_check("What is the largest word length ", "Invalid input! You did not provide an integer")
+wordLenMax = wrdMaxInput
+if wrdMaxInput < wrdMinInput:
+	wordLenMin = wrdMaxInput
+	wordLenMax = wrdMinInput
+# flips min/max values
+	print("shortest word length was changed to " + str(wordLenMin))
+print("longest word length is: " + str(wordLenMax))
+# set longest length
 
 def get_words():
 	wordList = []
 	wordFile = open("word_list.txt", "r")
-	#wordFile = ["turnbine", "dangerous", "apple", "ass", "fucking", "whenever", "asshole", "grassland", "assassin"]
 	for word in wordFile:
-		#print("RAW WORD IS: " + word)
 		clean_words(word, wordList, wordFile)
 	return wordList
 
 def clean_words(wrd, wrdArray, wrdSource):
 	strippedWord = wrd.strip('\n') # word defults adds \n
-	if len(strippedWord) >= wordLenMin and len(strippedWord) <= wordLenMax:
+	if len(strippedWord) >= int(wordLenMin) and len(strippedWord) <= int(wordLenMax):
 		wrdArray.append(strippedWord) 
 		#Adds trimmed words to list
 		for xxx in badWords:
-			#print("xxx is: " + xxx)
 			if xxx == strippedWord:
-				#print(xxx + " is a forbidden word")
 				wrdArray.remove(strippedWord)
 				#removes word if it matches with a forbidden word
 			elif xxx != strippedWord:
@@ -38,12 +59,9 @@ def clean_words(wrd, wrdArray, wrdSource):
 		return wrdArray
 
 words = get_words()
-#print(words)
 random.shuffle(words)
 selection = words[0:4]
 sep = '-'
+print('*' * 30)
 print(sep.join(selection))
-
-#print("minimum word length is: " + str(wordLenMin))
-#print("max word length is: " + str(wordLenMax))
 
